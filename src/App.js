15a9fdac2fea644deb24e179ea79632a5fa6ec4e
razielll <asto1387@gmail.com>
2024-03-23 21:57:05 -0400

    add styling

--- src/App.js
+++ src/App.js
@@ -3,36 +3,36 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [rate, setRate] = useState(10);
-  const [initialSum, setInitialSum] = useState(100);
-  const [timePeriod, setTimePeriod] = useState(90);
+  const [rate, setRate] = useState(7);
+  const [initialSum, setInitialSum] = useState(1000);
+  const [timePeriod, setTimePeriod] = useState(20);
 
   return (
     <div className="App">
-      <header>
+      <header className="site-title">
         <h1>Simple compound calculator</h1>
       </header>
       <main className="main-section">
         <div className="rate-input">
-          <label>Rate %</label>
-          <input type="number" value={rate} onChange={e => setRate(e.target.value)} />
+          <label for="rate">Rate %</label>
+          <input id="rate" type="number" value={rate} onChange={e => setRate(e.target.value)} />
           {!rate && (
             <div>
-            <span style={{ color: 'red' }}>Rate required</span>
+              <span style={{ color: 'red' }}>Rate required</span>
             </div>
           )}
         </div>
         <div className="rate-input">
-          <label>Base sum</label>
-          <input type="number" value={initialSum} onChange={e => setInitialSum(e.target.value)}/>
+          <label for="sum">Base sum</label>
+          <input id="sum" type="number" value={initialSum} onChange={e => setInitialSum(e.target.value)} />
         </div>
         <div className="rate-input">
-          <label>Number of compounds</label>
-          <input type="number" value={timePeriod} onChange={e => setTimePeriod(e.target.value)}/>
+          <label for="numcomp">Number of compounds</label>
+          <input id="numcomp" type="number" value={timePeriod} onChange={e => setTimePeriod(e.target.value)} />
         </div>
         <div className="result">
-        {parseInt(initialSum*(1+rate/100)**timePeriod).toLocaleString('en-us')}
-        {/* {(initialSum*(1+rate/100)**timePeriod).toLocaleString('en-us')} */}
+          {parseInt(initialSum * (1 + rate / 100) ** timePeriod).toLocaleString('en-us')}
+          {/* {(initialSum*(1+rate/100)**timePeriod).toLocaleString('en-us')} */}
           {/* result: {(initialSum*(1+rate/100)**timePeriod)} */}
           {/* {console.log(initialSum*(1+rate/100)**timePeriod)} */}
           {/* <div>rate: {rate}</div>
@@ -41,14 +41,18 @@ function App() {
         </div>
         <section className="how-to">
           <b>How to use</b>
-          <p>Let's assume that we start with 100$ and we add 10% to it 90 times in a compounding way, which is the default example</p>
-          <p>It will result that adding 10% to the initial sum of 100$, 90 times compounded, will net <b><u>531,302$</u></b></p>
+          <p>For example, let's say we make a 7% return on our 1000$ initial deposit, and we continue re-investing the money (with the gains!) and make another 7%, and do it again, for 20 times.</p>
+          <p>That's default site example</p>
         </section>
       </main>
       <footer>
-        <p>I was frustrated from most simple google searches for a simple compound calculator, so I've decided to make a small website to offer it at its basic form</p>
-        <p>It follows the simple formula of initialSum*(1 + Rate)<sup>#compound period</sup></p>
-        <p>I hope you find this useful and easy to use! Best, A.</p>
+        <h4>Personal note:</h4>
+        <p>I was frustrated from most simple google search results for a compound calculator, so I've decided to make a small website to offer it in its basic form for free</p>
+        <p>It follows the formula of <br />
+          <span className="formula">initialSum*(1 + Rate)<sup>#compound period</sup></span>
+        </p>
+        <p>I hope you find this useful and be surprised by the power of compound interest!</p>
+        <h5>Best, A.</h5>
       </footer>
     </div>
   );
